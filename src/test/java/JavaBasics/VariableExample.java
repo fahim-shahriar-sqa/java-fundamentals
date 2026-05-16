d16741b4746270e667ba7a47239f62ba04413aee
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2026-05-16 22:33:04 +0600

    Add expanded Java variable and data type examples

---
+++ src/test/java/JavaBasics/VariableExample.java
@@ -0,0 +1,25 @@
+package JavaBasics;
+
+public class VariableExample {
+    public static void main(String[] args) {
+
+        int number = 10;
+        float decimalNumber = 5.5f;
+        boolean isJavaFun = true;
+        String name = "Fahim";
+        char grade = 'A';
+
+        // Simple calculation
+        int result = number + 5;
+
+        System.out.println("Integer Value  : " + number);
+        System.out.println("Float Value    : " + decimalNumber);
+        System.out.println("Boolean Value  : " + isJavaFun);
+        System.out.println("String Value   : " + name);
+        System.out.println("Character Value: " + grade);
+
+        System.out.println("Updated Result : " + result);
+
+        System.out.println("Hello " + name + ", welcome to Java!");
+    }
+}
